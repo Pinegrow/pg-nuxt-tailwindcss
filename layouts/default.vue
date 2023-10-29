9d1b23875cf3788dfb6aac6e6f6cd14beb3c070a
Akayy <techakayy@gmail.com>
2023-10-29 22:52:19 +1100

    Minor edit

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,18 +3,20 @@
 </script>
 
 <template>
-  <!-- <div class="container mx-auto"> -->
-  <div
-    class="min-h-screen flex flex-col bg-white dark:bg-neutral-950 text-neutral-900 dark:text-neutral-50"
-  >
-    <!-- <NavBar class="fixed shadow z-20 bg-white dark:bg-neutral-950" />
+  <div>
+    <!-- <div class="container mx-auto"> -->
+    <div
+      class="min-h-screen flex flex-col bg-white dark:bg-neutral-950 text-neutral-900 dark:text-neutral-50"
+    >
+      <!-- <NavBar class="fixed shadow z-20 bg-white dark:bg-neutral-950" />
     <div class="mt-24"> -->
-    <main class="shadow">
-      <slot />
-    </main>
+      <main class="shadow">
+        <slot />
+      </main>
+      <!-- </div> -->
+      <!-- <TheFooter /> -->
+    </div>
     <!-- </div> -->
-    <!-- <TheFooter /> -->
   </div>
-  <!-- </div> -->
 </template>
 <style></style>
