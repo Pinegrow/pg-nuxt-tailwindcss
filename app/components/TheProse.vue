f68d83188c06a113b95585f96dee09120e6e0069
Akayy <techakayy@gmail.com>
2024-12-01 06:47:18 +1100

    Added TheProse component to template

---
+++ app/components/TheProse.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+<template>
+  <div
+    class="p-10 dark:prose-invert dark:prose-gray-100 flex flex-col heading-offset max-w-none prose prose-gray-800 rounded-lg"
+  >
+    <slot />
+  </div>
+</template>
+<style></style>
